07e185ba62aa411c76b1127f7199fca6cc1281ee
ChingizPro <chingizprog@gmail.com>
2024-08-10 21:10:29 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,24 @@
+const appsbtn = document.querySelector('.appsbtn')
+const apps = document.querySelector('.apps')
+const profilebtn = document.querySelector('.profilebtn')
+const profilesection = document.querySelector('.profilesection')
+
+appsbtn.addEventListener('click', () => {
+    if(apps.classList.contains('showapps'))
+        apps.classList.remove('showapps')
+    else
+        apps.classList.add('showapps')
+
+    if(profilesection.classList.contains('showprofilesection'))
+        profilesection.classList.remove('showprofilesection')
+})
+
+profilebtn.addEventListener('click', () => {
+    if(profilesection.classList.contains('showprofilesection'))
+        profilesection.classList.remove('showprofilesection')
+    else
+        profilesection.classList.add('showprofilesection')
+
+    if(apps.classList.contains('showapps'))
+        apps.classList.remove('showapps')
+})
